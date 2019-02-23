a624da9e5f2eda8d5d06b42ac0ad68753baeb5ff
Tomasz Kucybała <tkucybala@me.com>
2019-02-23 10:16:00 +0100

    testy pierwsze

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,3 @@
-package main;
-
 public class Calculator {
 
 	public double sum(double a, double b) {
@@ -11,6 +9,7 @@ public class Calculator {
 	}
 
 	public double multiply(double a, double b) {
+
 		return a * b;
 	}
 
